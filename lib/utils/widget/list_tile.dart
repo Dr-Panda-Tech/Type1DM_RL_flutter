13fe6b33f0ee60252d6fba7e5dba944bf753d87e
ForgetMeNotJun <junjun.umbreon0304@gmail.com>
2023-11-04 12:10:26 +0900

    [update]#13 tap action

--- lib/utils/widget/list_tile.dart
+++ lib/utils/widget/list_tile.dart
@@ -7,15 +7,22 @@ Widget customListTile({
 }) {
   return Container(
     alignment: Alignment.center,
-    child: ListTile(
-      contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-      leading: Icon(leadingIcon),
-      title: Text(
-        titleText,
-        style: const TextStyle(fontSize: 14),
+    child: Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: onTapAction,
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        child: ListTile(
+          contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+          leading: Icon(leadingIcon),
+          title: Text(
+            titleText,
+            style: const TextStyle(fontSize: 14),
+          ),
+          trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+        ),
       ),
-      trailing: const Icon(Icons.arrow_forward_ios, size: 16),
-      onTap: onTapAction,
     ),
   );
 }
