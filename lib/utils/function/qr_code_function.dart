bfda386daf0763fa62dab32de5f00925887d429d
Dr-Panda-Tech <dr.panda.liferecipe@gmail.com>
2023-11-10 15:41:19 +0900

    [add]qr generetor

---
+++ lib/utils/function/qr_code_function.dart
@@ -0,0 +1,67 @@
+import 'dart:ui';
+import 'dart:typed_data';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+Future<Uint8List?> generateQRCode(String data) async {
+  final qrValidationResult = QrValidator.validate(
+    data: data,
+    version: QrVersions.auto,
+    errorCorrectionLevel: QrErrorCorrectLevel.L,
+  );
+
+  if (qrValidationResult.status == QrValidationStatus.valid) {
+    final qrCode = qrValidationResult.qrCode;
+    final painter = QrPainter.withQr(
+      qr: qrCode!,
+      gapless: false,
+      embeddedImageStyle: null,
+      embeddedImage: null,
+    );
+    final picData = await painter.toImageData(2048, format: ImageByteFormat.png);
+    return picData?.buffer.asUint8List();
+  } else {
+    throw Exception('Unable to generate QR Code, invalid input data');
+  }
+}
+
+Future<void> uploadQRCode(Uint8List qrCode, String uid) async {
+  FirebaseStorage storage = FirebaseStorage.instance;
+  Reference ref = storage.ref().child('qrCodes/$uid.png');
+  await ref.putData(qrCode);
+}
+
+Future<void> generateAndUploadQRCode() async {
+  final user = await FirebaseAuth.instance.currentUser;
+  if (user != null) {
+    // ユーザーのUIDとメールアドレスを含むデータを生成
+    String data = 'uid: ${user.uid}, email: ${user.email}';
+    // QRコードを生成
+    Uint8List? qrCode = await generateQRCode(data);
+    if (qrCode != null) {
+      // QRコードをFirebaseにアップロード
+      await uploadQRCode(qrCode, user.uid);
+      // アップロード成功の通知
+      print('QR Code uploaded successfully!');
+    }
+  } else {
+    print('No user is signed in.');
+  }
+}
+
+Future<Uint8List?> getQRCode(String uid) async {
+  FirebaseStorage storage = FirebaseStorage.instance;
+  Reference ref = storage.ref().child('qrCodes/$uid.png');
+
+  try {
+    // Attempt to download data from Firebase Storage
+    final Uint8List? data = await ref.getData();
+    // If data is not null, return it
+    return data;
+  } catch (e) {
+    // If an error occurs, print it and return null
+    print('An error occurred while downloading the QR Code: $e');
+    return null;
+  }
+}
--- lib/views/record/record_page.dart
+++ lib/views/record/record_page.dart
@@ -16,102 +16,120 @@ class _RecordPageState extends State<RecordPage> {
   String? mealCategory;
   TextEditingController glucoseController = TextEditingController();
   bool _isLoading = false;
-  int? recommendationUnit;  // 変数名を変更
+  int? recommendationUnit; // 変数名を変更
+
+  void _unfocus() {
+    FocusScope.of(context).unfocus();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorConstants.backgroundColor,
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            customListTile(
-              leadingIcon: Icons.person,
-              titleText: '身長・体重を更新する',
-              onTapAction: () {
-                Navigator.pushNamed(context, '/demographicsMutableUpdatePage');
-              },
-            ),
-            customListTile(
-              leadingIcon: Icons.bloodtype,
-              titleText: '血液検査結果を入力する',
-              onTapAction: () {
-                Navigator.pushNamed(context, '/bloodTestPage');
-              },
-            ),
-            customListTile(
-              leadingIcon: Icons.medication_liquid,
-              titleText: 'インスリンを変更する',
-              onTapAction: () {
-                Navigator.pushNamed(context, '/insulinUpdatePage');
-              },
-            ),
-            DropdownButton<String>(
-              value: mealCategory,
-              hint: const Text(
-                '食事のカテゴリーを選択',
-                style: kLargeButtonTextStyle,
-              ), // ヒントテキストのフォントサイズを変更
-              onChanged: (String? newValue) {
-                setState(() {
-                  mealCategory = newValue;
-                });
-              },
-              items: <String>['大', '中', '小']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(
-                    value,
+      body: GestureDetector(
+        behavior: HitTestBehavior.opaque, //画面外タップを検知するために必要
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 150),
+                customListTile(
+                  leadingIcon: Icons.person,
+                  titleText: '身長・体重を更新する',
+                  onTapAction: () {
+                    Navigator.pushNamed(
+                        context, '/demographicsMutableUpdatePage');
+                  },
+                ),
+                customListTile(
+                  leadingIcon: Icons.bloodtype,
+                  titleText: '血液検査結果を入力する',
+                  onTapAction: () {
+                    Navigator.pushNamed(context, '/bloodTestPage');
+                  },
+                ),
+                customListTile(
+                  leadingIcon: Icons.medication_liquid,
+                  titleText: 'インスリンを変更する',
+                  onTapAction: () {
+                    Navigator.pushNamed(context, '/insulinUpdatePage');
+                  },
+                ),
+                DropdownButton<String>(
+                  value: mealCategory,
+                  hint: const Text(
+                    '食事のカテゴリーを選択',
                     style: kLargeButtonTextStyle,
-                  ), // アイテムのフォントサイズを変更
-                );
-              }).toList(),
-              itemHeight: 60, // アイテムの高さを変更
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: glucoseController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: '直前の血糖値',
-                border: OutlineInputBorder(),
-              ),
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.digitsOnly, // 数値のみを許可
+                  ), // ヒントテキストのフォントサイズを変更
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      mealCategory = newValue;
+                    });
+                  },
+                  items: <String>['大', '中', '小']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(
+                        value,
+                        style: kLargeButtonTextStyle,
+                      ), // アイテムのフォントサイズを変更
+                    );
+                  }).toList(),
+                  itemHeight: 60, // アイテムの高さを変更
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: glucoseController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: '直前の血糖値',
+                    border: OutlineInputBorder(),
+                  ),
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly, // 数値のみを許可
+                  ],
+                  onEditingComplete: _unfocus,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    setState(() {
+                      _isLoading = true;
+                    });
+                    await Future.delayed(const Duration(seconds: 2));
+                    recommendationUnit = calculateRecommendation(); // 関数名を変更
+                    saveGlucoseInsulinToFirestore(
+                      mealCategory: mealCategory,
+                      glucose: glucoseController.text,
+                      recommendationUnit: recommendationUnit, // 変数名を変更
+                    );
+                    setState(() {
+                      _isLoading = false;
+                    });
+                  },
+                  style: elevatedButtonStyle,
+                  child: const Text(
+                    '単位数を計算する',
+                    style: kLargeButtonTextStyle,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                if (_isLoading)
+                  const CircularProgressIndicator(), // ローディング中はアニメーションを表示
+                if (recommendationUnit != null && !_isLoading)
+                  Text(
+                    '推奨単位数: $recommendationUnit単位',
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                const SizedBox(height: 250),// 計算後は推奨インスリン単位を表示
               ],
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                setState(() {
-                  _isLoading = true;
-                });
-                await Future.delayed(const Duration(seconds: 2));
-                recommendationUnit = calculateRecommendation();  // 関数名を変更
-                saveGlucoseInsulinToFirestore(
-                  mealCategory: mealCategory,
-                  glucose: glucoseController.text,
-                  recommendationUnit: recommendationUnit,  // 変数名を変更
-                );
-                setState(() {
-                  _isLoading = false;
-                });
-              },
-              style: elevatedButtonStyle,
-              child: const Text('単位数を計算する', style: kLargeButtonTextStyle,),
-            ),
-            const SizedBox(height: 20),
-            if (_isLoading)
-              const CircularProgressIndicator(),  // ローディング中はアニメーションを表示
-            if (recommendationUnit != null && !_isLoading)
-              Text(
-                '推奨単位数: $recommendationUnit単位',
-                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),  // 計算後は推奨インスリン単位を表示
-          ],
+          ),
         ),
       ),
     );
